10cc28e22e732827b0b4eb157ad539c8e8646f31
MateoID <m.isazad@uniandes.edu.co>
2025-02-05 19:36:43 -0500

    scraping BD con R

---
+++ scripts/1_ImportCleaningData.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(rvest)
+
+tabla_geih <- data.frame()
+
+for (i in 1:10){
+  my_url <- sprintf("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_%d.html", i)
+  my_table <- read_html(my_url) %>% 
+    html_elements(xpath='/html/body/table') %>% 
+    html_table()
+  
+  my_table <- as.data.frame(my_table)
+  
+  tabla_geih <- rbind(tabla_geih, my_table)
+}
